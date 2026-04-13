6be5d3d7c0f1c3ec9217ae55e9ea707e9dac50ce
Karryun <gdy0210@naver.com>
2026-04-13 14:54:25 +0900

    style: prefix 사용을 통한 코드 개선

--- src/main/java/com/example/study/domain/auth/util/AuthorizationUtils.java
+++ src/main/java/com/example/study/domain/auth/util/AuthorizationUtils.java
@@ -5,9 +5,11 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AuthorizationUtils {
+    private static final String PREFIX = "Bearer ";
+
     public static String getAccessToken(String authHeader) {
-        if (authHeader != null && authHeader.startsWith("Bearer ")) {
-            return authHeader.substring(7);
+        if (authHeader != null && authHeader.startsWith(PREFIX)) {
+            return authHeader.substring(PREFIX.length());
         }
         throw new IllegalArgumentException("토큰에 문제가 있습니다.");
     }
